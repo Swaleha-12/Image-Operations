d67168a650e2a55120f7648f4288f70338ea9bd4
Swaleha Saleem <sualehasaleem12@gmail.com>
2020-02-10 00:23:57 +0500

    final code with comments

--- src/image_operations.py
+++ src/image_operations.py
@@ -37,11 +37,9 @@ class PointerList:
     def __init__(self, size: int, value=None) -> None:
         """Creates a list of the given size, optionally intializing elements to value.
         The list is static. It only has space for size elements.
-
         Args:
         - size: size of the list; space is reserved for these many elements. 
         - value: the optional initial value of the created elements.
-
         Returns:
         none
         """
@@ -55,11 +53,8 @@ class PointerList:
 
     def __len__(self) -> int:
         '''Returns the size of the list. Allows len() to be called on it.
-
         Ref: https://stackoverflow.com/q/7642434/1382487
-
         Args:
-
         Returns:
         the size of the list.
         '''
@@ -67,12 +62,9 @@ class PointerList:
 
     def __getitem__(self, i: int):
         '''Returns the value at index, i. Allows indexing syntax.
-
         Ref: https://stackoverflow.com/a/33882066/1382487
-
         Args:
         - i: the index from which to retrieve the value.
-
         Returns:
         the value at index i.
         '''
@@ -86,13 +78,10 @@ class PointerList:
 
     def __setitem__(self, i: int, value) -> None:
         '''Sets the element at index, i, to value. Allows indexing syntax.
-
         Ref: https://stackoverflow.com/a/33882066/1382487
-
         Args:
         - i: the index of the elemnent to be set
         - value: the value to be set
-
         Returns:
         none
         '''
@@ -106,11 +95,8 @@ class PointerList:
 
     def __iter__(self) -> MyListIterator:
         '''Returns an iterator that allows iteration over this list.
-
         Ref: https://thispointer.com/python-how-to-make-a-class-iterable-create-iterator-class-for-it/
-
         Args:
-
         Returns:
         an iterator that allows iteration over this list.
         '''
@@ -118,12 +104,9 @@ class PointerList:
 
     def get(self, i: int):
         '''Returns the value at index, i.
-
         Alternate to use of indexing syntax.
-
         Args:
         - i: the index from which to retrieve the value.
-
         Returns:
         the value at index i.
         '''
@@ -131,13 +114,10 @@ class PointerList:
 
     def set(self, i: int, value) -> None:
         '''Sets the element at index, i, to value.
-
         Alternate to use of indexing syntax.
-
         Args:
         - i: the index of the elemnent to be set
         - value: the value to be set
-
         Returns:
         none
         '''
@@ -149,13 +129,10 @@ class ArrayList:
 
     def __init__(self, size: int, value=None) -> None:
         """Creates a list of the given size, optionally intializing elements to value.
-
         The list is static. It only has space for size elements.
-
         Args:
         - size: size of the list; space is reserved for these many elements. 
         - value: the optional initial value of the created elements.
-
         Returns:
         none
         """
@@ -170,11 +147,8 @@ class ArrayList:
 
     def __len__(self) -> int:
         '''Returns the size of the list. Allows len() to be called on it.
-
         Ref: https://stackoverflow.com/q/7642434/1382487
-
         Args:
-
         Returns:
         the size of the list.
         '''
@@ -182,12 +156,9 @@ class ArrayList:
 
     def __getitem__(self, i: int):
         '''Returns the value at index, i. Allows indexing syntax.
-
         Ref: https://stackoverflow.com/a/33882066/1382487
-
         Args:
         - i: the index from which to retrieve the value.
-
         Returns:
         the value at index i.
         '''
@@ -198,13 +169,10 @@ class ArrayList:
 
     def __setitem__(self, i: int, value) -> None:
         '''Sets the element at index, i, to value. Allows indexing syntax.
-
         Ref: https://stackoverflow.com/a/33882066/1382487
-
         Args:
         - i: the index of the elemnent to be set
         - value: the value to be set
-
         Returns:
         none
         '''
@@ -217,11 +185,8 @@ class ArrayList:
 
     def __iter__(self) -> MyListIterator:
         '''Returns an iterator that allows iteration over this list.
-
         Ref: https://thispointer.com/python-how-to-make-a-class-iterable-create-iterator-class-for-it/
-
         Args:
-
         Returns:
         an iterator that allows iteration over this list.
         '''
@@ -229,12 +194,9 @@ class ArrayList:
 
     def get(self, i: int):
         '''Returns the value at index, i.
-
         Alternate to use of indexing syntax.
-
         Args:
         - i: the index from which to retrieve the value.
-
         Returns:
         the value at index i.
         '''
@@ -242,13 +204,10 @@ class ArrayList:
 
     def set(self, i: int, value) -> None:
         '''Sets the element at index, i, to value.
-
         Alternate to use of indexing syntax.
-
         Args:
         - i: the index of the elemnent to be set
         - value: the value to be set
-
         Returns:
         none
         '''
@@ -261,10 +220,8 @@ class MyImage:
 
     def __init__(self, size: (int, int), pointer=False) -> None:
         """Initializes a black image of the given size.
-
         Args:
         - size: (width, height) specifies the dimensions to create.
-
         Returns:
         none
         """
@@ -279,14 +236,11 @@ class MyImage:
 
     def _get_index(self, r: int, c: int) -> int:
         """Returns the list index for the given row, column coordinates.
-
         This is an internal function for use in class methods only. It should
         not be used or called from outside the class.
-
         Args:
         - r: the row coordinate
         - c: the column coordinate
-
         Returns:
         the list index corresponding to the given row and column coordinates
         """
@@ -298,13 +252,10 @@ class MyImage:
 
     def open(path: str, pointer=False):
         """Creates and returns an image containing from the information at file path.
-
         The image format is inferred from the file name. The read image is
         converted to RGB as our type only stores RGB.
-
         Args:
         - path: path to the file containing image information
-
         Returns:
         the image created using the information from file path.
         """
@@ -322,12 +273,9 @@ class MyImage:
 
     def save(self, path: str) -> None:
         """Saves the image to the given file path.
-
         The image format is inferred from the file name.
-
         Args:
         - path: the image has to be saved here.
-
         Returns:
         none
         """
@@ -338,11 +286,9 @@ class MyImage:
 
     def get(self, r: int, c: int) -> (int, int, int):
         """Returns the value of the pixel at the given row and column coordinates.
-
         Args:
         - r: the row coordinate
         - c: the column coordinate
-
         Returns:
         the stored RGB value of the pixel at the given row and column coordinates.
         """
@@ -350,12 +296,10 @@ class MyImage:
 
     def set(self, r: int, c: int, rgb: (int, int, int)) -> None:
         """Write the rgb value at the pixel at the given row and column coordinates.
-
         Args:
         - r: the row coordinate
         - c: the column coordinate
         - rgb: the rgb value to write
-
         Returns:
         none
         """
@@ -363,9 +307,7 @@ class MyImage:
 
     def show(self) -> None:
         """Display the image in a GUI window.
-
         Args:
-
         Returns:
         none
         """
@@ -378,59 +320,54 @@ class MyImage:
 def remove_channel(src: MyImage, red: bool = False, green: bool = False,
                    blue: bool = False) -> MyImage:
     """Returns a copy of src in which the indicated channels are suppressed.
-
     Suppresses the red channel if no channel is indicated. src is not modified.
-
     Args:
     - src: the image whose copy the indicated channels have to be suppressed.
     - red: suppress the red channel if this is True.
     - green: suppress the green channel if this is True.
     - blue: suppress the blue channel if this is True.
-
     Returns:
     a copy of src with the indicated channels suppressed.
     """
-    localImage = MyImage(src.size, src.pointer)
+    localImage = MyImage(src.size, src.pointer) #creating a new image so as to keep the 
+                                                #original image intact
 
     for i in range(src.size[1]):
         for j in range(src.size[0]):
             pixel = src.get(i, j)
             if red or not (green or blue):
-                pixel = (0, pixel[1], pixel[2])
+                pixel = (0, pixel[1], pixel[2])     #suppressing the red channel
             if green:
-                pixel = (pixel[0], 0, pixel[2])
+                pixel = (pixel[0], 0, pixel[2])     #suppressing the green channel
             if blue:
-                pixel = (pixel[0], pixel[1], 0)
+                pixel = (pixel[0], pixel[1], 0)     #suppressing the blue channel
             localImage.set(i, j, pixel)
     return localImage
 
 
 def rotations(src: MyImage) -> MyImage:
     """Returns an image containing the 4 rotations of src.
-
     The new image has twice the dimensions of src. src is not modified.
-
     Args:
     - src: the image whose rotations have to be stored and returned.
-
     Returns:
     an image twice the size of src and containing the 4 rotations of src.
     """
-    result = MyImage((src.size[0]*2, src.size[1]*2), src.pointer)
+    result = MyImage((src.size[0]*2, src.size[1]*2), src.pointer)   #creating an image twice the original size
     rotated: MyImage = degrees90(src)
     for count in range(4):
         for i in range(src.size[0]):
             for j in range(src.size[1]):
                 x = rotated.get(i, j)
                 if count == 0:
-                    result.set(i, j, x)
+                    result.set(i, j, x)                         #set the pixels on top left
                 elif count == 1:
-                    result.set(i+src.size[1], j, x)
+                    result.set(i+src.size[1], j, x)             #set the pixels on bottom left
                 elif count == 2:
-                    result.set(i+src.size[1], j+src.size[1], x)
+                    result.set(i+src.size[1], j+src.size[1], x) #set the pixels on bottom right
                 elif count == 3:
-                    result.set(i, j+src.size[1], x)
-        if count < 3:
+                    result.set(i, j+src.size[1], x)             #set the pixels on top right
+        if count < 3: 
             rotated = degrees90(rotated)
     return result
 
@@ -439,27 +376,24 @@ def degrees90(locimage: MyImage) -> MyImage:
     n = locimage.size[0]
     for x in range(0, int(n/2)):
         for y in range(x, n-x-1):
-            temp = locimage.get(x, y)
-            locimage.set(x, y, locimage.get(y, n-1-x))
-            locimage.set(y, n-1-x, locimage.get(n-1-x, n-1-y))
-            locimage.set(n-1-x, n-1-y, locimage.get(n-1-y, x))
-            locimage.set(n-1-y, x, temp)
+            temp = locimage.get(x, y)                           # store current pixel in temp variable
+            locimage.set(x, y, locimage.get(y, n-1-x))          # move values from right to top
+            locimage.set(y, n-1-x, locimage.get(n-1-x, n-1-y))  # move values from bottom to right
+            locimage.set(n-1-x, n-1-y, locimage.get(n-1-y, x))  # move values from left to bottom 
+            locimage.set(n-1-y, x, temp)                        # assign temp to left 
     return locimage
 
 
 def apply_mask(src: MyImage, maskfile: str, average: bool = True) -> MyImage:
     """Returns an copy of src with the mask from maskfile applied to it.
-
     maskfile specifies a text file which contains an n by n mask. It has the
     following format:
     - the first line contains n
     - the next n^2 lines contain 1 element each of the flattened mask
-
     Args:
     - src: the image on which the mask is to be applied
     - maskfile: path to a file specifying the mask to be applied
     - average: if True, averaging should to be done when applying the mask
-
     Returns:
     an image which the result of applying the specified mask to src.
     """
